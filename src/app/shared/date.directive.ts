958265e1ecf5bec54e643d89bad11dc08339e1f0
3ndrius <andrzejo1991@gmail.com>
2019-11-15 06:41:10 +0100

    Directives v2

--- src/app/shared/date.directive.ts
+++ src/app/shared/date.directive.ts
@@ -1,14 +1,18 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appDate]'
 })
 export class DateDirective {
 
+@Input()
+  private date: Date;
+
   constructor() { }
   @HostListener('mouseenter')
   mouseenter(eventDate: Event) {
     console.log('mouseenter');
+    console.log(this.date);
   }
   @HostListener('mouseleave')
   mouseleave(eventDate: Event) {
