46f51be5b6bfd72a34564cfa83ee39c0bbd4acf8
3ndrius <andrzejo1991@gmail.com>
2019-11-15 06:26:14 +0100

    Using dirictives to set style

--- src/app/shared/checked.directive.ts
+++ src/app/shared/checked.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[appChecked]'
 })
 export class CheckedDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
 
+  ngOnInit(): void {
+      let li = this.el.nativeElement;
+      this.renderer.setStyle(li, 'color', 'navy');
+
+  }
 }
