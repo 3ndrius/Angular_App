a791ff615b37735bb7a896d0d9c31cd8a2103c9c
3ndrius <andrzejo1991@gmail.com>
2019-11-15 06:18:14 +0100

    Adding first directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { ListTaskComponent } from './list-task/list-task.component';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { TaskDoneComponent } from './task-done/task-done.component';
 import { TasksService } from './tasks.service';
+import { CheckedDirective } from './shared/checked.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListTaskComponent,
     AddTaskComponent,
-    TaskDoneComponent
+    TaskDoneComponent,
+    CheckedDirective
   ],
   imports: [
   BrowserModule,
